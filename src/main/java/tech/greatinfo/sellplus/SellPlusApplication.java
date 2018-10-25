ffdd5031ba5a7de7ad9ba94c815c79062cc94334
Mr.Ma <812916310@qq.com>
2018-10-25 16:45:44 +0800

    修改vue路由配置

--- src/main/java/tech/greatinfo/sellplus/SellPlusApplication.java
+++ src/main/java/tech/greatinfo/sellplus/SellPlusApplication.java
@@ -2,6 +2,11 @@ package tech.greatinfo.sellplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class SellPlusApplication {
@@ -9,4 +14,19 @@ public class SellPlusApplication {
     public static void main(String[] args) {
         SpringApplication.run(SellPlusApplication.class, args);
     }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+
+                container.addErrorPages(error404Page);
+            }
+        };
+    }
+
 }
