7db9ca8355cbc78f028bc750ec17dbd4eeb99003
Mr.Ma <812916310@qq.com>
2018-10-29 17:23:45 +0800

    代码回滚

--- src/main/java/tech/greatinfo/sellplus/domain/coupons/Coupon.java
+++ src/main/java/tech/greatinfo/sellplus/domain/coupons/Coupon.java
@@ -1,13 +1,7 @@
 package tech.greatinfo.sellplus.domain.coupons;
 
+import javax.persistence.*;
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
 
 /**
  *
